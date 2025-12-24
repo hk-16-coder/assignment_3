629342429615cf6d7ec5916fca2325d6d1b6c47f
hk-16-coder <hamzakothari1978@gmail.com>
2025-12-24 12:29:31 +0530

    Add files via upload

---
+++ assign-3_task2.py
@@ -0,0 +1,11 @@
+import math
+n = int(input("Enter a number :  "))
+
+if n>0:
+ print(f"Square root : {math.sqrt(n)}")
+ print(f"Logarithm : {math.log(n)}")
+
+else:
+ print("Enter a positive number")
+
+print(f"Sine : {math.sin(n)}")
